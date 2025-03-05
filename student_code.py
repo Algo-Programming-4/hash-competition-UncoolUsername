20e3c4f5df0fbb1c235e8b44d65cef35a880ed31
GavinR <riggigav000@alpinesd.org>
2025-03-05 16:37:37 -0700

    Add files via upload

---
+++ student_code.py
@@ -0,0 +1,11 @@
+from hash import hashTable as Hash
+hash_table = Hash()
+
+def words_in(words):
+    t = hash_table.words_in(words)
+    return t
+
+
+def lookup_word_count(argumentThingys): # TODO: Change this name later
+    tt = hash_table.lookup_word_count(argumentThingys)
+    return tt
